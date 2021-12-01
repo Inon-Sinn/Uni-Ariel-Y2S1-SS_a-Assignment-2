7bd440c4ca078708cfd0be5c67f9b3463dabb0a9
Inon Sinn <inon.sinn@gmail.com>
2021-12-01 11:18:50 +0200

    working project

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 
 int main(){
 
-    char c;
-    int unfinshed = 1; 
-    while(unfinshed){
-        scanf("%c",&c);
-        if(c=='A')
-            collect_The_Mat();
-        if(c=='B')
-            is_There_A_Path();
-        if(c=='C')
-            shortest_Path();
-        if(c=='D')
-            unfinshed = 0;
-    }
+   char c;
+   int unfinshed = 1;
+   while(unfinshed){
+       scanf("%c",&c);
+       if(c=='A')
+           collect_The_Mat();
+       if(c=='B')
+           is_There_A_Path();
+       if(c=='C')
+           shortest_Path();
+       if(c=='D')
+           unfinshed = 0;
+   }
     printf("finished the run\n");
 
 
--- my_mat.c
+++ my_mat.c
@@ -1,58 +1,81 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-int mat[10][10];
-int shortest_path_mat[10][10];
-int created;
+#define len 10
+int mat[len][len];
+int max=0;
 
 int min(int a, int b){
+    if(a==0 && b==0)
+        return 0;
+    if(a==0)
+        return b;
+    if(b==0)
+        return a;
     if(a>=b)
         return b;
     return a; 
 }
 
-void collect_The_Mat(){
+void printMatrix(){
     int i=0,j=0;
-    for(i=0;i<10;i++){
-        for(j=0;j<10;j++)
+    for(i=0;i<len;i++){
+        for(j=0;j<len;j++)
+            printf("%d,",mat[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+
+
+
+void collect_The_Mat(){
+    //Collect the matrix - works
+    int i=0,j=0;
+    for(i=0;i<len;i++){
+        for(j=0;j<len;j++){
             scanf("%d",&mat[i][j]);
+            if(i!=j && mat[i][j]==0)
+                mat[i][j]=-1;
+        }
     }
-    i=0,j=0;
-    for(i=0;i<10;i++){
-        for(j=0;j<10;j++)
-            shortest_path_mat[i][j] = mat[i][j];
+    int k,n,m;
+    for(k=0;k<len;k++){
+        for(n=0;n<len;n++){
+            for(m=0;m<len;m++){
+                if(n!=k && m!=k && n!=m){
+                    int a = mat[n][m];
+                    int b = mat[n][k];
+                    int c = mat[k][m];
+                    if(a==-1){
+                        if(b==-1 || c==-1)
+                            mat[n][m] =-1;
+                        else mat[n][m] = b+c;
+                    }
+                    else if(b==-1 || c==-1)
+                            mat[n][m] =a;
+                    else mat[n][m] = min(a,b+c);
+                }
+            }
+        }
     }
-    created = 0;
-    printf("filled\n");
 }
 
 void is_There_A_Path(){
     int i,j;
     scanf("%d%d",&i,&j);
-    if(mat[i][j]!=0)
+    if(mat[i][j]!=-1 && mat[i][j]!=0){
         printf("True\n");
+    }
     else printf("False\n");
 }
 
 void shortest_Path(){
     int i,j;
     scanf("%d%d",&i,&j);
-    int result;
-    if(created==0){
-        created=1;
-        int k,n,m;
-        for(k=0;k<10;k++){
-            for(n=0;n<10;n++){
-                for(m=0;m<10;m++){
-                    int a = shortest_path_mat[i][j];
-                    int b = shortest_path_mat[i][k];
-                    int c = shortest_path_mat[k][j];
-                    shortest_path_mat[i][j] = min(a,b+c);
-                }
-            }
-        }
-    }
-    result=shortest_path_mat[i][j];
+    int result=mat[i][j];
     if(result==0)
         printf("-1\n");
     else printf("%d\n",result);
